3732707106f015c2b9c9249d5b72fc671297cc0f
dangngo18 <danghaingo.work@gmail.com>
2026-03-25 17:01:46 +0700

    feat: Define task reducer action types and update README.md.

--- src/features/task/types/reducer.ts
+++ src/features/task/types/reducer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { TaskState } from "./state";
 import type { Task } from ".";
